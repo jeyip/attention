9ec17e3b5d27491469d9a6182d93513c48a075e1
Jeremy Yip <jeremy.yip@automattic.com>
2020-08-23 19:33:26 -0700

    Add auth0 auth functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import { domain, authZeroClientId } from "./constants";
+import logo from "./logo.svg";
+import "./App.css";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Auth0Provider
+      domain={domain}
+      clientId={authZeroClientId}
+      redirectUri={window.location.origin}
+    >
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit s<code>src/App.js</code> and save to reload.
+          </p>
+          <LoginButton />
+          <LogoutButton />
+        </header>
+      </div>
+    </Auth0Provider>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const authZeroSecret = process.env.AUTH_ZERO_SECRET;
+export const authZeroClientId = process.env.AUTH_ZERO_CLIENT_ID;
+export const domain = "northstar-webapp.auth0.com";
