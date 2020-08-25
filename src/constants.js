582e6123f6ecbf2d21f34172a5c3b97dbfac9fec
Jeremy Yip <jeremy.yip@automattic.com>
2020-08-24 19:21:36 -0700

    Update env variable names

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const authZeroSecret = process.env.AUTH_ZERO_SECRET;
-export const authZeroClientId = process.env.AUTH_ZERO_CLIENT_ID;
+export const authZeroSecret = process.env.REACT_APP_AUTH_ZERO_SECRET;
+export const authZeroClientId = process.env.REACT_APP_AUTH_ZERO_CLIENT_ID;
 export const domain = "northstar-webapp.auth0.com";
