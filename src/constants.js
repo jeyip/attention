02edfd88fd8eda7e425554f5de6b87312f516c00
Jeremy Yip <jeremy.yip@automattic.com>
2020-08-30 19:47:51 -0700

    Add new lifx token content

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const authZeroSecret = process.env.REACT_APP_AUTH_ZERO_SECRET;
 export const authZeroClientId = process.env.REACT_APP_AUTH_ZERO_CLIENT_ID;
+export const lifxToken = process.env.REACT_APP_LIFX_TOKEN;
 export const domain = "northstar-webapp.auth0.com";
